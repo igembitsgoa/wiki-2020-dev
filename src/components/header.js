270198c3582d3087f599b7a8b46e2735013a7337
Pranav Ballaney <ballaneypranav@gmail.com>
2020-04-07 01:34:51 +0530

    Added css module for header

--- src/components/header.js
+++ src/components/header.js
@@ -2,28 +2,13 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import headerStyles from "./header.module.scss"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header className={headerStyles.header}>
+    <div className={headerStyles.container}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
           {siteTitle}
         </Link>
       </h1>
--- src/components/layout.js
+++ src/components/layout.js
@@ -35,13 +35,7 @@ const Layout = ({ children }) => {
           crossorigin="anonymous"></link>
       </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div class="container">
         <main>{children}</main>
         <Footer />
       </div>
