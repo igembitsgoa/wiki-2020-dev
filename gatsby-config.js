a744ef462ca0ac516b5fa363f5bb05e7e67a960b
Pranav Ballaney <ballaneypranav@gmail.com>
2020-04-08 02:07:34 +0530

    Added google-fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,17 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`500`, `600`]
+          },
+        ],
+      },
+    }
   ],
 }
