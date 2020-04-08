09848ab74716d395e9694213410d52191b45724b
Pranav Ballaney <ballaneypranav@gmail.com>
2020-04-09 04:52:03 +0530

    Add basic fullscreen nav

--- src/components/header.js
+++ src/components/header.js
@@ -7,22 +7,40 @@ import headerStyles from "./header.module.scss"
 const Header = () => (
   <>
     <header>
-      <nav className={["navbar navbar-expand-lg navbar-light", `${headerStyles.bgGreen}`].join(" ")}>
-        <div className="container">
-          <div className="d-flex">
-            <Link className={["navbar-brand d-lg-inline-block text-white", `${headerStyles.navbarBrand}`].join(" ")}>
-              iGEM BITS Goa
+      <nav className={["navbar navbar-expand-lg d-flex justify-content-between", `${headerStyles.bgGreen}`].join(" ")}>
+        <div class="ml-4">
+          <Link className={["navbar-brand d-lg-inline-block text-white", `${headerStyles.navbarBrand}`].join(" ")}>
+            iGEM BITS Goa
           </Link>
-          </div>
-          <div className="d-flex align-items-right">
-            <ul className="navbar-nav ml-auto flex-nowrap">
-              <li className="nav-item">
-                <Link className={[`${headerStyles.menuItem}`, "nav-link m-2 menu-item text-uppercase text-white"].join(" ")}>
-                  Explore
-              </Link>
-              </li>
-            </ul>
-          </div>
+        </div>
+        <div class="mr-3 mb-2">
+          <div id="menuArea">
+            <input type="checkbox" id="menuToggle"
+                   className={[`${headerStyles.input}`, `${headerStyles.menuToggle}`].join(" ")}></input>
+
+            {/* <label for="menuToggle" className={[`${headerStyles.menuItem}`, 
+                                          "menuOpen nav-link text-uppercase text-white"].join(" ")}>
+                     <Link id="menuToggle" className={[`${headerStyles.menuItem}`,
+                     "nav-link m-2 menu-item text-uppercase text-white open"].join(" ")}> 
+                      Explore
+                    </label> */}
+
+            <label for="menuToggle" className={headerStyles.menuOpen}>
+              <div className={headerStyles.open}></div>
+            </label>
+
+            <div className={[`${headerStyles.menu}`, `${headerStyles.menuEffects}`].join(" ")}>
+              <label for="menuToggle" class="mr-3"></label>
+              <div className={headerStyles.menuContent}>
+                <ul>
+                  <li><Link>MENU 1</Link></li>
+                  <li><Link>MENU 2</Link></li>
+                  <li><Link>MENU 3</Link></li>
+                  <li><Link>MENU 4</Link></li>
+                </ul>
+              </div>
+            </div>
+          </div >
         </div>
       </nav>
     </header>
@@ -38,3 +56,5 @@ Header.defaultProps = {
 }
 
 export default Header
+
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Helmet from "react-helmet"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
