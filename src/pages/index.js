61efd779293a47160998956d92f021d159d30028
Pranav Ballaney <ballaneypranav@gmail.com>
2020-04-12 01:27:54 +0530

    Move image code from image.js to individual pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,47 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { graphql } from 'gatsby'
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import indexStyles from "../components/index.module.scss"
 
-const IndexPage = () => (
+
+const IndexPage = (props) => (
   <>
     <Helmet>
       <title>iGEM BITS Goa 2020</title>
     </Helmet>
     <Layout>
-      <div className="container" style={{ height: 800 + 'px' }}>
+      <main className="container" style={{ height: 800 + 'px' }}>
         <div className="row h-100 w-100 align-items-center text-center">
           <div className="col">
-            <Image />
+            <div className={indexStyles.logo}>
+              <Img fluid={props.data.logoCenter.childImageSharp.fluid} />
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </Layout>
   </>
 )
 
 export default IndexPage
+
+export const fluidImage = graphql`
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 1000) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+`;
+
+export const pageQuery = graphql`
+  query {
+    logoCenter: file(relativePath: { eq: "igembitslogo.png" }) {
+      ...fluidImage
+    }
+  }
+`
